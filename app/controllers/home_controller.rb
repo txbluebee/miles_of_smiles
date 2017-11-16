1804c40c6499f2bddac8dbdde5854f4f077e4d52
Brian Fan <kuomu.fan@yahoo.com>
2017-11-16 15:42:04 -0800

    edit meta tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,4 @@
 class HomeController < ApplicationController
   def index
-    set_meta_tags title: 'Home',
-                  description: 'eCommerce Photography Websites',
-                  og: {
-                    title:    "Home",
-                    description: "eCommerce Photography Websites",
-                    type:     'website',
-                    url:      '/',
-                    image:    'https://images.unsplash.com/photo-1500027419059-afa3360ead5d?auto=format&fit=crop&w=1050&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'
-                  }
   end
 end
