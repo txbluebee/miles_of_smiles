ab557c1ea9d26322cc17ba4c0154fa39de6ada2b
Brian Fan <kuomu.fan@yahoo.com>
2017-11-16 15:34:10 -0800

    edit meta link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   def index
-    set_meta_tags title: 'Miles of Smiles',
+    set_meta_tags title: 'Home',
                   description: 'eCommerce Photography Websites',
                   og: {
-                    title:    "Miles of Smiles",
+                    title:    "Home",
                     description: "eCommerce Photography Websites",
                     type:     'website',
                     url:      '/',
-                    image:    'http://lorempixel.com/320/240/cats'
+                    image:    'https://images.unsplash.com/photo-1500027419059-afa3360ead5d?auto=format&fit=crop&w=1050&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'
                   }
   end
 end
