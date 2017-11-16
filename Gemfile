acc493ad797eaa79857419eca0e94016db8e47c3
Brian Fan <kuomu.fan@yahoo.com>
2017-11-16 15:03:31 -0800

    add meta tags

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'faker'
 gem 'devise'
 gem 'paperclip'
 gem 'aws-sdk', '~> 2.3'
+gem 'meta-tags'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    set_meta_tags title: 'Home',
+                  description: 'eCommerce Photography Websites',
+                  og: {
+                    title:    "Home",
+                    description: "eCommerce Photography Websites",
+                    type:     'website',
+                    url:      '/',
+                    image:    'cover.jpg'
+                  }
   end
 end
