e109541d9095d22abebf5c5b13c1feab665f1010
Brian Fan <kuomu.fan@yahoo.com>
2017-11-16 15:13:42 -0800

    edit meta tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   def index
-    set_meta_tags title: 'Home',
+    set_meta_tags title: 'Miles of Smiles',
                   description: 'eCommerce Photography Websites',
                   og: {
                     title:    "Home",
                     description: "eCommerce Photography Websites",
                     type:     'website',
                     url:      '/',
-                    image:    'cover.jpg'
+                    image:    '<%= image_path("images/cover.jpg") %>'
                   }
   end
 end
